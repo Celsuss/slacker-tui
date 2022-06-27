7c38dbf395aa215ed8fa917e68a166d6313cce8f
celsuss <jens.lorden@imagimob.com>
2022-06-27 14:23:38 +0200

    Added an Observer and Notifier, still need testing

---
+++ src/observer.rs
@@ -0,0 +1,33 @@
+
+// #[derive(Copy)]
+pub enum Event { 
+    Tick,
+    Quit,
+    ChangeConversation(String),
+}
+
+pub trait Observer{
+    fn notify(&self, event: &Event);
+}
+
+pub struct Notifier {
+    observers: Vec<Box<dyn Observer>>,
+}
+
+impl Notifier {
+    pub fn new() -> Self {
+        Notifier {
+            observers: Vec::new(),
+        }
+    }
+
+    pub fn add_observer(&mut self, observer: Box<dyn Observer>) {
+        self.observers.push(observer);
+    }
+
+    pub fn notify_observers<T>(&self, event: &Event) {
+        for observer in &self.observers {
+            observer.notify(event);
+        }
+    }
+}
