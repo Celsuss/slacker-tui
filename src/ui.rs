c5521ae00d9d5b18ce50c0a73eb6eb5db5c25837
celsuss <jens.lorden@imagimob.com>
2022-07-11 18:56:08 +0200

    Can now draw channels, users and teams in new ui file

--- src/ui.rs
+++ src/ui.rs
@@ -11,7 +11,9 @@ use tui::{
 };
 
 use crate::util;
-use crate::windows::App;
+use crate::windows::{
+    App, ActiveBlock,
+};
 
 pub fn draw_ui<B: Backend>(frame: &mut Frame<B>, app: &App<'_>)
 -> Result<(), Box<dyn std::error::Error>> {
@@ -51,23 +53,70 @@ where
         )
         .split(chunk);
 
-    // draw_teams(frame, app, channel_chunks[0]);
+    draw_teams(frame, app, channel_chunks[0]);
     draw_channels(frame, app, channel_chunks[1]);
     // draw_users(frame, app, channel_chunks[2]);
+}
+
+pub fn draw_teams<B>(frame: &mut Frame<B>, app: &App<'_>, chunk: Rect)
+where
+B: Backend{
+    let title = "Teams";
+
+    // Temp teams
+    let team_list = vec!["Imagimob", "Other"];
+    // Get Teams
+    let items: Vec<_> = team_list.iter()
+        .map(|team| team.to_owned())
+        .collect();
+
+    let highlight_state = (
+        app.active_block == ActiveBlock::Channels,
+        app.hovered_block == ActiveBlock::Channels,
+    );
+
+    draw_selectable_list(frame, app, chunk, title, &items,
+        highlight_state, app.selected_team_index);
 }
 
 pub fn draw_channels<B>(frame: &mut Frame<B>, app: &App<'_>, chunk: Rect)
 where
 B: Backend{
-    // Get channels
+    let title = "Channels";
 
-    let items: Vec<_> = app.channel_list.conversation_list.iter()
-        .map(|channel| {
-            ListItem::new(Spans::from(vec![Span::styled(
-                channel.name.clone(),
-                Style::default(),
-            )]))
-        }).collect();
+    // Get channels
+    let items: Vec<_> = app.channel_list.iter()
+        .map(|item| item.name.to_owned())
+        .collect();
+
+    // let current_route = app.get_current_route();
+    let highlight_state = (
+        app.active_block == ActiveBlock::Channels,
+        app.hovered_block == ActiveBlock::Channels,
+    );
+
+    draw_selectable_list(frame, app, chunk, title, &items,
+        highlight_state, app.selected_channel_index);
+}
+
+pub fn draw_users<B>(frame: &mut Frame<B>, app: &App<'_>, chunk: Rect)
+where
+B: Backend{
+    let title = "Users";
+
+    // Get users
+    let items: Vec<_> = app.user_list.iter()
+        .map(|item| item.name.to_owned())
+        .collect();
+
+    // let current_route = app.get_current_route();
+    let highlight_state = (
+        app.active_block == ActiveBlock::Users,
+        app.hovered_block == ActiveBlock::Users,
+    );
+
+    draw_selectable_list(frame, app, chunk, title, &items,
+        highlight_state, app.selected_user_index);
 }
 
 pub fn draw_conversation<B>(frame: &mut Frame<B>, app: &App<'_>, chunk: Rect)
